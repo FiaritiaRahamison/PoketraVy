e43c9d2a6374006adc19a5a55f800a44092f4830
Fiaritia <rahamisonf@gmail.com>
2024-06-24 09:05:38 +0300

    CRUD mouvement.

---
+++ PoketraVy-frontoffice/Controllers/MouvementController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PoketraVy_frontoffice.Data;
+using PoketraVy_frontoffice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoketraVy_frontoffice.Controllers
+{
+    public class MouvementController : Controller
+    {
+        private readonly MouvementRepository _mouvementRepository;
+
+        public MouvementController(MouvementRepository mouvementRepository)
+        {
+            _mouvementRepository = mouvementRepository;
+        }
+
+        // GET: MouvementController
+        public ActionResult Index()
+        {
+            var mouvements = _mouvementRepository.GetAll();
+            return View(mouvements);
+        }
+
+        // GET: MouvementController/Details/5
+        public ActionResult Details(int id)
+        {
+            var mouvement = _mouvementRepository.GetById(id);
+            if (mouvement == null)
+            {
+                return NotFound();
+            }
+
+            return View(mouvement);
+        }
+
+        // GET: MouvementController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MouvementController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Mouvement mouvement)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _mouvementRepository.Add(mouvement);
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to create record: {ex.Message}");
+            }
+            return View(mouvement);
+        }
+
+        // GET: MouvementController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var mouvement = _mouvementRepository.GetById(id);
+            if (mouvement == null)
+            {
+                return NotFound();
+            }
+
+            return View(mouvement);
+        }
+
+        // POST: MouvementController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Mouvement mouvement)
+        {
+            try
+            {
+                _mouvementRepository.Update(mouvement);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MouvementController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var mouvement = _mouvementRepository.GetById(id);
+            if (mouvement == null)
+            {
+                return NotFound();
+            }
+
+            return View(mouvement);
+        }
+
+        // POST: MouvementController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                _mouvementRepository.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PoketraVy-frontoffice/Data/MouvementRepository.cs
@@ -0,0 +1,169 @@
+﻿using PoketraVy_frontoffice.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoketraVy_frontoffice.Data
+{
+    public class MouvementRepository
+    {
+        private readonly string _connectionString;
+
+        public MouvementRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        // Get all Mouvement records
+        public IEnumerable<Mouvement> GetAll()
+        {
+            var mouvements = new List<Mouvement>();
+
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var command = new SqlCommand("SELECT * FROM mouvement", connection);
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            mouvements.Add(new Mouvement
+                            {
+                                ID = (int)reader["ID"],
+                                IdCategorieUtilisateurBudget = (int)reader["IdCategorieUtilisateurBudget"],
+                                Designation = reader["Designation"].ToString(),
+                                montant = (double)reader["Montant"],
+                                daty = (DateTime)reader["Daty"]
+                            });
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Error: {ex.Message}");
+                throw;
+            }
+
+            return mouvements;
+        }
+
+        // Get a single mouvement by ID
+        public Mouvement GetById(int id)
+        {
+            Mouvement mouvement = null;
+
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var command = new SqlCommand("SELECT * FROM mouvement WHERE ID = @ID", connection);
+                    command.Parameters.AddWithValue("@ID", id);
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            mouvement = new Mouvement
+                            {
+                                ID = (int)reader["ID"],
+                                IdCategorieUtilisateurBudget = (int)reader["IdCategorieUtilisateurBudget"],
+                                Designation = reader["Designation"].ToString(),
+                                montant = (double)reader["Montant"],
+                                daty = (DateTime)reader["Daty"]
+                            };
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Error: {ex.Message}");
+                throw;
+            }
+
+            return mouvement;
+        }
+
+        // Add a new mouvement
+        public void Add(Mouvement mouvement)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var command = new SqlCommand(
+                        "INSERT INTO mouvement (IdCategorieUtilisateurBudget, Designation, montant, daty) " +
+                        "VALUES (@IdCategorieUtilisateurBudget, @Designation, @montant, @daty)",
+                        connection);
+
+                    command.Parameters.AddWithValue("@IdCategorieUtilisateurBudget", mouvement.IdCategorieUtilisateurBudget);
+                    command.Parameters.AddWithValue("@Designation", mouvement.Designation);
+                    command.Parameters.AddWithValue("@montant", (double)mouvement.montant);
+                    command.Parameters.AddWithValue("@daty", (DateTime)mouvement.daty);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Error: {ex.Message}");
+                throw;
+            }
+        }
+
+        // Update an existing mouvement
+        public void Update(Mouvement mouvement)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var command = new SqlCommand(
+                        "UPDATE mouvement SET IdCategorieUtilisateurBudget = @IdCategorieUtilisateurBudget, Designation = @Designation, montant = @montant, daty = @daty " +
+                        "WHERE ID = @ID",
+                        connection);
+
+                    command.Parameters.AddWithValue("@ID", mouvement.ID);
+                    command.Parameters.AddWithValue("@IdCategorieUtilisateurBudget", mouvement.IdCategorieUtilisateurBudget);
+                    command.Parameters.AddWithValue("@Designation", mouvement.Designation);
+                    command.Parameters.AddWithValue("@montant", (double)mouvement.montant);
+                    command.Parameters.AddWithValue("@daty", (DateTime)mouvement.daty);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Error: {ex.Message}");
+                throw;
+            }
+        }
+
+        // Delete a mouvement by ID
+        public void Delete(int id)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connectionString))
+                {
+                    var command = new SqlCommand("DELETE FROM mouvement WHERE ID = @ID", connection);
+                    command.Parameters.AddWithValue("@ID", id);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"SQL Error: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
